7eb4f638e6599283a3c68f4ff25a36dcbcdaf18a
DanielWFetterman <dwf5277@psu.edu>
2023-10-19 13:24:20 -0400

    block and text

--- psu-block/src/psu-block.js
+++ psu-block/src/psu-block.js
@@ -5,32 +5,45 @@ const logo = new URL('../assets/open-wc-logo.svg', import.meta.url).href;
 class PsuBlock extends LitElement {
   static properties = {
     header: { type: String },
+    boxText: {type: String },
+
   }
 
-  static styles = css`
+  static get styles () {
+    return css`
+    
     :host {
       display: inline-block;
       
 
     }
 
-    
-  `;
+    .block {
+      padding: 8px;
+      border: 2px solid #444;
+      width: 403.531px ;
+      height: 342.250px; 
+      background-color: #1e407c;
+      color: white;
+      font-size: 32px;
+    }
+
+    `;
+
+  } 
 
   constructor() {
     super();
     this.header = 'My app';
+    this.boxText = this.boxText || "default";
   }
 
   render() {
     return html`
-      <main>
-        
 
-
-
-      </main>
-
+    <div class="block" >
+      <h3> ${this.boxText} </h3>
+    </div> 
       
     `;
   }
